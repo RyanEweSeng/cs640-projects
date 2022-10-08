7a663d3b5fa18455ba07058a5c596b47dd0393f8
RyanEweSeng <ryantoh1001@gmail.com>
2022-10-08 17:06:22 -0500

    Implement handle packets

--- lab2-switch-router/src/edu/wisc/cs/sdn/vnet/rt/Router.java
+++ lab2-switch-router/src/edu/wisc/cs/sdn/vnet/rt/Router.java
@@ -5,6 +5,7 @@ import edu.wisc.cs.sdn.vnet.DumpFile;
 import edu.wisc.cs.sdn.vnet.Iface;
 
 import net.floodlightcontroller.packet.Ethernet;
+import net.floodlightcontroller.packet.IPv4;
 
 /**
  * @author Aaron Gember-Jacobson and Anubhavnidhi Abhashkumar
@@ -15,6 +16,11 @@ public class Router extends Device {
 	
 	/** ARP cache for the router */
 	private ArpCache arpCache;
+
+	/**
+	 * Flag to trigger debug statements (remember to set to false on submission).
+	 */
+	private boolean dbg = true;
 	
 	/**
 	 * Creates a router for a specific host.
@@ -71,13 +77,89 @@ public class Router extends Device {
 	 * @param inIface the interface on which the packet was received
 	 */
 	public void handlePacket(Ethernet etherPacket, Iface inIface) {
-		System.out.println("*** -> Received packet: " +
-                etherPacket.toString().replace("\n", "\n\t"));
+		if (dbg) {
+			System.out.println("###########################################################");
+			System.out.println("hostname: " + getHost());
+			System.out.println("*** -> Received packet: " + etherPacket.toString().replace("\n", "\n\t") + "\n");
+		}
+
+		if (dbg) System.out.print("checking ether type...");
+
+		// check if it contains an IPv4 packet
+		if (etherPacket.getEtherType() != Ethernet.TYPE_IPv4) {
+			if (dbg) System.out.println("not IPv4...dropping packet");
+
+			return;
+		}
+
+		// get the payload
+		IPv4 header = (IPv4) etherPacket.getPayload();
+
+		// store the checksum and zero it
+		short checksum = header.getChecksum();
+		header = header.setChecksum((short) 0);
+
+		// compute the checksum
+		byte[] serializedData = header.serialize();
+		header = (IPv4) header.deserialize(serializedData, 0, serializedData.length);
+		short computedChecksum = header.getChecksum();
 		
-		/********************************************************************/
-		/* TODO: Handle packets                                             */
-	 	routeTable.lookup(167772518);
-		
-		/********************************************************************/
+		if (dbg) System.out.print("\nchecking checksum...");
+
+		// verify checksum
+		if (checksum != computedChecksum) {
+			if (dbg) System.out.println("checksum mismatch...dropping packet");
+
+			return;
+		}
+
+		if (dbg) System.out.print("\nchecking ttl...");
+
+		// decrement the IPv4 packet’s TTL by 1 and verify
+		header = header.setTtl((byte)(header.getTtl() - 1));
+		if (header.getTtl() == 0){
+			if (dbg) System.out.println("invalid ttl...dropping packet");
+
+			return;
+		}
+
+		if (dbg) System.out.print("\nchecking destination IP...");
+
+		// check if destination IP exactly matches one of the interface's IP
+		int headerDestIP = header.getDestinationAddress();
+		for (Iface iface : interfaces.values()) {
+			if (iface.getIpAddress() == headerDestIP) {
+				if (dbg) System.out.println("exact match found...dropping packet");
+
+				return;
+			}
+		}
+
+		if (dbg) System.out.print("\nsearching route table...");
+
+		// Forward packet
+		RouteEntry re = routeTable.lookup(headerDestIP);
+		if (re != null) {
+			if (dbg) System.out.println("entry found");
+
+			// determine the next-hop IP address and lookup the MAC address corresponding to that IP address from the ARP cache
+			ArpEntry ae = null;
+			if (re.getGatewayAddress() != 0) {
+				// destination IP is in another network and we need to move across routers to reach it
+				ae = arpCache.lookup(re.getGatewayAddress());
+			} else {
+				ae = arpCache.lookup(headerDestIP);
+			}
+			// updated the Ethernet header with the new source and destination MACs
+			header = header.setSourceAddress(re.getInterface().getMacAddress());
+			header = header.setDestinationAddress(ae.getMac());
+
+
+			// send packet
+		} else {
+			if (dbg) System.out.println("no entry found...dropping packet");
+
+			return;
+		}
 	}
 }
