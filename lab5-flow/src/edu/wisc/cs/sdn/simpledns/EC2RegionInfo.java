35858172bb19102210589486ada13df0eca214c9
RyanEweSeng <ryantoh1001@gmail.com>
2022-12-14 19:25:28 -0600

    improved code

--- lab5-flow/src/edu/wisc/cs/sdn/simpledns/EC2RegionInfo.java
+++ lab5-flow/src/edu/wisc/cs/sdn/simpledns/EC2RegionInfo.java
@@ -1,15 +1,23 @@
 package edu.wisc.cs.sdn.simpledns;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.nio.ByteBuffer;
+
 class EC2RegionInfo {
-    private String location;
     private long ip;
     private long mask;
+    private String location;
 
     EC2RegionInfo(String entry) {
         String[] tokens = entry.split("[,/]");
-        this.location = tokens[3];
-        this.ip = parseIp(tokens[0]);
+        try {
+            this.ip = parseIp(tokens[0]);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
         this.mask = parsePrefix(Integer.parseInt(tokens[1]));
+        this.location = tokens[2];
     }
 
     public long getIp() {
@@ -24,15 +32,19 @@ class EC2RegionInfo {
         return this.location;
     }
 
-    private static long parseIp(String ipString) {
-        String[] ipStringParts = ipString.split("\\.");
-        int[] ipValParts = new int[4];
+    private static long parseIp(String ipString) throws UnknownHostException {
+        InetAddress addr = InetAddress.getByName(ipString);
+        long longRepresentation = ByteBuffer.wrap(addr.getAddress()).getInt();
 
-        for (int i = 0; i < ipValParts.length; i++) {
-            ipValParts[i] = Integer.parseInt(ipStringParts[i]);
-        }
+        return longRepresentation;
+        // String[] ipStringParts = ipString.split("\\.");
+        // int[] ipValParts = new int[4];
 
-        return ipValParts[0] * (2^24) + ipValParts[1] * (2^16) + ipValParts[2] * (2^8) + ipValParts[3];
+        // for (int i = 0; i < ipValParts.length; i++) {
+        //     ipValParts[i] = Integer.parseInt(ipStringParts[i]);
+        // }
+
+        // return ipValParts[0] * (2^24) + ipValParts[1] * (2^16) + ipValParts[2] * (2^8) + ipValParts[3];
     }
 
     // reference: https://stackoverflow.com/questions/2942299/converting-cidr-address-to-subnet-mask-and-network-address
